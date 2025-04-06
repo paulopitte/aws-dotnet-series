a271eafd4d43b76345a0c43af6cad3a1167291dc
Paulo Pitte <pauloanalista@outlook.com>
2025-04-06 15:11:19 -0300

    DynamoDBTable

---
+++ amazon-dynamoDB/DynamoDbStudentDemo/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+namespace DynamoStudentManager.Models;
+    [DynamoDBTable("students")]
+    public class Student
+    {
+        [DynamoDBHashKey("id")]
+        public int? Id { get; set; }
+
+        [DynamoDBProperty("firstname")]
+        public string? FirstName { get; set; }
+
+        [DynamoDBProperty("lastname")]
+        public string? LastName { get; set; }
+
+      
+    }
+
